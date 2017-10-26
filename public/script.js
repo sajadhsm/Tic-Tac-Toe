6871499c9fc5d902e78d43cc105fcbf73898ffd2
Sajad Hashemian <sajadhsm@gmail.com>
2017-10-26 16:05:52 +0330

    Add Menu options

--- public/script.js
+++ public/script.js
@@ -1,14 +1,39 @@
 // Global Selectors
+const options = document.querySelector('#options');
+const choose = document.querySelector('#choose');
 const board = document.querySelector('#board');
 const buttons = document.querySelectorAll('.flx');
 
-let turn = 'X';
+// Hide game board at game start
+board.style.display = 'none';
+choose.style.display = 'none';
+
+function multiPlayer() {
+    options.style.display = 'none';
+    chooseTurn();
+    
+}
+
+function chooseTurn() {
+    choose.style.display = 'flex';
+    const xo = document.querySelectorAll('.xo div');
+    xo.forEach(div => {
+        div.addEventListener('click', () => {
+            turn = div.innerHTML;
+            choose.style.display = 'none';
+            board.style.display = 'flex';
+        });
+    });
+}
+
+
+let turn;
 let total = 0;
 
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
         const childTxt = btn.firstElementChild;
-
+        
         if (childTxt.innerHTML === '') {
             childTxt.innerHTML = turn;
             turn = turn === 'X' ? 'O' : 'X';
@@ -27,3 +52,8 @@ function totalCheck(btns) {
         total = 0;
     } else total++;
 }
+
+
+// document.querySelector('.single').addEventListener('click', singlePlayer);
+document.querySelector('.multi').addEventListener('click', multiPlayer);
+// document.querySelector('.online').addEventListener('click', onlineGame);
